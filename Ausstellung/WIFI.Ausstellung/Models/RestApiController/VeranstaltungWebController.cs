9dfc52bce9a5c1179b4cf34dc0d34e2b31a1cdf4
zenoxart <81261706+zenoxart@users.noreply.github.com>
2021-06-17 18:39:38 +0200

    Newtonsoft und WebController

---
+++ Ausstellung/WIFI.Ausstellung/Models/RestApiController/VeranstaltungWebController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WIFI.Ausstellung.Models.RestApiController
+{
+    /// <summary>
+    /// Stellt einen Dienst zum Verwalten der Veranstaltung über die Rest-Api
+    /// </summary>
+    public class VeranstaltungWebController : WIFI.Anwendung.ViewModelAppObjekt
+    {
+        /// <summary>
+        /// Erstellt einen Eintrag unter dem Table Veranstaltung in der Datenbank, falls noch keiner existiert
+        /// </summary>
+        public async void ErstelleVeranstaltung()
+        {
+            const string Adresse = "{0}ErstelleVeranstaltung";
+
+            using (var Antwort = await this.HttpClient.GetAsync(
+                   string.Format(
+                       Adresse,
+                       Properties.Settings.Default.UrlGatewayAPI
+                       )))
+            {
+
+            }
+        }
+    }
+}
