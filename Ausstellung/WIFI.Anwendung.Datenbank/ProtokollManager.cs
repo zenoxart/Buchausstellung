f92bd92018f9aa80eecd1589b88f818845147fbe
zenoxart <81261706+zenoxart@users.noreply.github.com>
2021-07-22 15:35:15 +0200

    errorfix

--- Ausstellung/WIFI.Anwendung.Datenbank/ProtokollManager.cs
+++ Ausstellung/WIFI.Anwendung.Datenbank/ProtokollManager.cs
@@ -582,8 +582,8 @@ namespace WIFI.Anwendung
                     {
                         using (var Schreiber = new System.IO.StreamWriter(
                             this.Pfad,
-                            append: true,
-                            System.Text.Encoding.Default))
+                            append: true
+                            ))
                         {
 
                             const string Ausgabemuster = "{0}\t{1}\t{2}";
